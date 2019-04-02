31c5bb8c0aa6e592463506946a43ef15e44e5303
Albrecht Weiche <uodnk@student.kit.edu>
2019-04-02 11:13:56 +0200

    blincking LED

---
+++ gdb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+echo "Please run openocd in another terminal window (you might need sudo)"
+echo ""
+
+# https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+unameOut="$(uname -s)"
+gdb -iex 'add-auto-load-safe-path .' $1
